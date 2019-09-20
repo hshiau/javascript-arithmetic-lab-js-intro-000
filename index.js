6329a4132aeb0083411d4e4d4115819b9c96c029
Howard Shiau <howardshiau@yahoo.com>
2019-09-20 21:44:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function divide(f1,f2) {
   return f1/f2
 }
 
+function inc(f1) {
+  return f1 += 1
+}
+
+function dec(f1) {
+  return f2 -= 1
+}
 function makeInt(f1) {
   return parseInt(f1,10)
 }
